f6d7d4bcb4a1a994683d8a89505456e8750298b5
Parthpal <parthapradippal@gmail.com>
2024-05-26 01:09:54 +0530

    order creation

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 import app from "./app";
 import config from "./app/config";
-//trycatc
+//tr-ycatch
 async function main() {
   try {
     await mongoose.connect(config.database_url as string);
