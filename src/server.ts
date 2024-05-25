16143030c834e1abfcda3cbb59acf93a6a216c65
Parthpal <parthapradippal@gmail.com>
2024-05-26 00:58:04 +0530

    create new product completed

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 import app from "./app";
 import config from "./app/config";
-
+//trycatch
 async function main() {
   try {
     await mongoose.connect(config.database_url as string);
